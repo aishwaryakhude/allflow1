f64e16f0e3fc752a3d3e4fb0b2abb1d106012aba
aishwaryakhude <97588291+aishwaryakhude@users.noreply.github.com>
2022-01-24 19:04:43 +0530

    Add files via upload

---
+++ src/main/java/test/testmain.java
@@ -0,0 +1,14 @@
+package test;
+
+public class testmain {
+
+	
+	public static void main(String[] args) {
+		System.out.println(isevenno(22));
+	}
+
+	public static boolean isevenno(int i) {
+		
+		return i%2==0;
+	}
+}
---
+++ src/test/java/test/Testtype.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Testtype {
+
+	@Test
+	public void test()
+	{
+		
+		System.out.println("Hello");
+		assertTrue(testmain.isevenno(22));
+	}
+	
+}
